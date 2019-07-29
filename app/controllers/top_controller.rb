05eb296cdb6311b89e953d75196d0e445a7dc06a
wabisachin <wabisachin1122@yahoo.co.jp>
2019-07-29 03:05:01 +0000

    マイページのタブ内のレイアウト変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
      def index
-          @posts = Post.page(params[:page]).per(5)
+          @posts = Post.order(created_at: "DESC").page(params[:page]).per(5)
      end
 end
